5f5f132f61d657300f248cc588f83b50dcdac111
Ivan-Navas <ivanrng.work@gmail.com>
2024-09-25 15:57:51 -0500

    feat: :sparkles: add route to get profile user

---
+++ src/app/api/user/auth/[id]/route.ts
@@ -0,0 +1,49 @@
+import { prisma } from "@/libs/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export const GET = async ({ params }: any) => {
+  try {
+    const userId = params.id;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(userId),
+      },
+      include: {
+        post: true,
+        followers: true,
+        following: true,
+      },
+    });
+
+    if (user) {
+      return NextResponse.json({
+        status: "success",
+        message: "Usuario",
+        user: {
+          id: user?.id,
+          name: user?.name,
+          email: user?.email,
+          nickName: user?.nickName,
+          image: user?.image,
+          post: user?.post,
+          follower: user?.followers,
+          following: user?.following,
+        },
+      });
+    }else{
+
+    return NextResponse.json({
+      status: "error",
+      message: "El usuario no existe",
+    });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      status: "error",
+      message: "Ocurrio un error",
+      error,
+    });
+  }
+};
