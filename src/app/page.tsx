8fbf3ce075297cc60a22ff6e6e1358d8ed761eb6
Ivan-Navas <ivanrng.work@gmail.com>
2024-10-23 10:14:30 -0500

    fix: :bug: redirect to another page in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect } from "react";
+import Cookies from "js-cookie";
+import { Auth } from "@/helpers/interfaces/user";
 
 export default function Home() {
-  return (
-    <main>
-     
-    </main>
-  );
+  useEffect(() => {
+    const cookies = Cookies.get("auth");
+    const auth: Auth = cookies ? JSON.parse(cookies) : null;
+    if (auth) {
+      window.location.href = "/page/feed";
+    } else {
+      window.location.href = "/page/login";
+    }
+  }, []);
+  return <main></main>;
 }
