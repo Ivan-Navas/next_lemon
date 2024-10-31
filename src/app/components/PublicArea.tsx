253fb353ec6c97099c5571beb2b2340c21da1c95
Ivan-Navas <ivanrng.work@gmail.com>
2024-10-31 18:13:38 -0500

    feat: :sparkles: add request to create publication in frontend

--- src/app/components/PublicArea.tsx
+++ src/app/components/PublicArea.tsx
@@ -1,21 +1,25 @@
 "use client";
+import { Auth } from "@/helpers/interfaces/user";
 import React, { useState } from "react";
 import { BiPaperclip, BiSolidSend } from "react-icons/bi";
+import Cookies from "js-cookie";
 
 function PublicArea() {
+  const cookies = Cookies.get("auth");
+  const auth: Auth = cookies ? JSON.parse(cookies) : null;
   const [file, setFile] = useState<any>(null);
+  const [publiData, setPubliData] = useState<string>();
   return (
     <form
       className="w-100 mt-13 grid"
       onSubmit={async (e) => {
         e.preventDefault();
-        const formData = new FormData();
-        formData.append("file", file);
-        const request = await fetch("/api/publication/create", {
+        const request = await fetch(`/api/publication/create/${auth.id}`, {
           method: "POST",
-          body: formData,
+          body: JSON.stringify({ data: publiData }),
         });
         const data = await request.json();
+        console.log(data);
       }}
     >
       <textarea
@@ -24,6 +28,9 @@ function PublicArea() {
         id="data"
         autoComplete="off"
         className="w-100 h-92 rounded-t-16 pl-13 pt-8 bg-lBackMain text-title text-12 font-medium mb-0 outline-none resize-none dark:bg-title dark:text-lFontMain dark:placeholder:text-lFontMain"
+        onChange={(e) => {
+          setPubliData(e.target.value);
+        }}
       ></textarea>
       <div className="bg-lBackMain rounded-b-16 mt-0 flex justify-end dark:bg-title">
         <input
