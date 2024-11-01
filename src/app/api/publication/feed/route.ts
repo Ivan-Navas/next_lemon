84c97295be85e8bdf3cc4f4d909ce49e2e3b7572
Ivan-Navas <ivanrng.work@gmail.com>
2024-11-01 13:09:24 -0500

    fix: :bug: show new post first

--- src/app/api/publication/feed/route.ts
+++ src/app/api/publication/feed/route.ts
@@ -2,27 +2,30 @@ import { prisma } from "@/libs/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export const GET = async (req: NextApiRequest)=>{
+export const GET = async (req: NextApiRequest) => {
   try {
-  const feed = await prisma.post.findMany(
-    {include: {
-      author: true,
-      comment: true,
-      share: true,
-      like: true,
-      view: true,
-    }}
-  )
-  return NextResponse.json({
-    status: "success",
-    message: "Feed",
-    feed
-  })
+    const feed = await prisma.post.findMany({
+      include: {
+        author: true,
+        comment: true,
+        share: true,
+        like: true,
+        view: true,
+      },
+      orderBy: {
+        date: "desc",
+      },
+    });
+    return NextResponse.json({
+      status: "success",
+      message: "Feed",
+      feed,
+    });
   } catch (error) {
     return NextResponse.json({
       status: "success",
       message: "ocurrioó un error",
-      error
-    })
+      error,
+    });
   }
-}
+};
